e11aa815551ecb8016978845fd1ecf313850f91e
jainigo <jainigo@profile.es>
2022-07-06 11:00:25 +0200

    updates

--- src/main/java/com/codependent/booth2/repository/CustomerRepository.java
+++ src/main/java/com/codependent/booth2/repository/CustomerRepository.java
@@ -3,5 +3,5 @@ package com.codependent.booth2.repository;
 import com.codependent.booth2.entity.CustomerEntity;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 
-public interface CustomerRepository extends ReactiveCrudRepository<CustomerEntity, String> {
+public interface CustomerRepository extends ReactiveCrudRepository<CustomerEntity, Integer> {
 }
